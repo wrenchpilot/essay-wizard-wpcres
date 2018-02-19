6708cdc9ad12d51dda118abc18d394e66d6701f6
wrenchpilot <wrenchpilot@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-02-19 18:49:26 +0000

    code formatting
    git-svn-id: https://plugins.svn.wordpress.org/essay-wizard-wpcres/trunk@1824759 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- script/wpcres-dropdown.js
+++ script/wpcres-dropdown.js
@@ -1,20 +1,23 @@
 // Create tinyMCE plugin for wpCRES Assingments dropdown menu
-tinymce.PluginManager.add( 'wpcres' , function( editor ){
+tinymce.PluginManager.add('wpcres', function (editor, url) {
 	//Grab the list of wpCRES assignments
 	//Put into JSON array
 	var value_array = [];
-	for (var i in my_wpcres_posts){
-		value_array.push({text:my_wpcres_posts[i][0],value: my_wpcres_posts[i][1]});
+	for (var i in my_wpcres_posts) {
+		value_array.push({
+			text: my_wpcres_posts[i][0],
+			value: my_wpcres_posts[i][1]
+		});
 	}
-	
+
 	//Add button
-    editor.addButton('wpcres', {
-        type: 'listbox',
+	editor.addButton('wpcres', {
+		type: 'listbox',
 		max_width: 300,
-        text: 'wpCRES Assignments',
-        onselect: function(ed) {
-            tinymce.execCommand('mceInsertContent',false,'[wpcres id="'+this.value()+'"]');
-        },
-        values: value_array
-    });
+		text: 'wpCRES Assignments',
+		onselect: function (ed) {
+			tinymce.execCommand('mceInsertContent', false, '[wpcres id="' + this.value() + '"]');
+		},
+		values: value_array
+	});
 });
